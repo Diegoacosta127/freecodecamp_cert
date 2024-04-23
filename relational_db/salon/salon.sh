a95faa2d287a938c070780aaf2b4fb5bcfc4e91d
diegoacosta127 <diegoacosta127@gmail.com>
2024-04-23 04:29:33 -0300

    First commit

---
+++ relational_db/salon/salon.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n\nWelcome to My Salon, how can I help you?\n"
+
+MAIN_MENU(){
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+  SERVICES=$($PSQL "SELECT * FROM services")
+  # display services available
+  echo "$SERVICES" | while read SERVICE_ID BAR NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
+  read SERVICE_ID_SELECTED
+  SERVICES_AVAILABLE=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  # if service selected is not available
+  if [[ -z $SERVICES_AVAILABLE ]]
+  then
+    MAIN_MENU "I could not find that service. What would you like today?"
+  else
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+    CUSTOMER=$($PSQL "SELECT * FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    if [[ -z $CUSTOMER ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+    else
+      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    fi
+    echo -e "\nWhat time would you like your cut, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+    read SERVICE_TIME
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
+  fi
+}
+MAIN_MENU
